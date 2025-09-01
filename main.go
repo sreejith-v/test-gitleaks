2790b2ffcf14b0b9ccf0ab30c90d107cdf6e11a9
Sreejith <sreejithv13055@gmail.com>
2025-09-01 14:32:41 +0530

    add token in a valid file

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmYWtlVXNlcklkIiwibmFtZSI6IkZha2UgVXNlciIsImlhdCI6MTY1MDAwMDAwMH0.ZW5kcG9pbnRfaXNfZmFrZV9zZWNyZXQ"
+
 func main() {
 	fmt.Println("Hello, World!")
-	secretToken := GenerateFakeJWT()
+	//secretToken := GenerateFakeJWT()
+	secretToken := token
 	fmt.Println("Secret Token:", secretToken)
 }
