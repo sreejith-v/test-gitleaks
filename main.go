7bac339e8ea666437340c9c04dd1c359af08e24f
Sreejith <sreejithv13055@gmail.com>
2025-09-01 14:33:32 +0530

    reuse generate token

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import "fmt"
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmYWtlVXNlcklkIiwibmFtZSI6IkZha2UgVXNlciIsImlhdCI6MTY1MDAwMDAwMH0.ZW5kcG9pbnRfaXNfZmFrZV9zZWNyZXQ"
-
 func main() {
 	fmt.Println("Hello, World!")
-	//secretToken := GenerateFakeJWT()
-	secretToken := token
+	secretToken := GenerateFakeJWT()
 	fmt.Println("Secret Token:", secretToken)
 }
