3f78f69095aed4155b198fcc0bb00df7271d96ae
Sreejith <sreejithv13055@gmail.com>
2025-09-01 14:33:32 +0530

    reuse generate token

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import "fmt"
 
-const token = "REMOVED_TOKEN"
-
 func main() {
 	fmt.Println("Hello, World!")
-	//secretToken := GenerateFakeJWT()
-	secretToken := token
+	secretToken := GenerateFakeJWT()
 	fmt.Println("Secret Token:", secretToken)
 }
