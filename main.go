2a265e6744192baa7735b6b0de8c2a6e6df13453
Sreejith <sreejithv13055@gmail.com>
2025-09-01 14:10:59 +0530

    add and use token const

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import "test-gitleaks/secrets"
 
 func main() {
 	fmt.Println("Hello, World!")
+	secretToken := secrets.SecretToken
+	fmt.Println("Secret Token:", secretToken)
 }
