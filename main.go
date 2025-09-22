a0f6c49c41e6c0e9363cf7582123a590d68d695c
Amar Singh Rathour <singhamarsingh555@gmail.com>
2025-09-22 11:04:01 +0530

    add JWT token and PGP keys for authentication

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	token := "REMOVED_TOKEN"
 	fmt.Println("Hello, World!")
+	fmt.Println(token)
 	secretToken := GenerateFakeJWT()
 	fmt.Println("Secret Token:", secretToken)
 }
